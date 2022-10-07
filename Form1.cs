03c7327fffe7fdf8337599c90e7732a7e18f5a5b
mttn550 <matanw@mail.com>
2022-10-07 15:38:43 +0300

    גרפיקה
    גרפיקה של קלפים

--- Form1.cs
+++ Form1.cs
@@ -19,6 +19,37 @@ namespace HOKM
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Image img = pictureBox9.BackgroundImage;
+            img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox9.BackgroundImage = img;
+
+            img = pictureBox10.BackgroundImage;
+            img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            pictureBox10.BackgroundImage = img;
+
+            img = pictureBox11.BackgroundImage;
+            img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            pictureBox11.BackgroundImage = img;
+
+            img = pictureBox12.BackgroundImage;
+            img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            pictureBox12.BackgroundImage = img;
+
+            img = pictureBox13.BackgroundImage;
+            img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox13.BackgroundImage = img;
+
+            img = pictureBox14.BackgroundImage;
+            img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox14.BackgroundImage = img;
+
+            img = pictureBox15.BackgroundImage;
+            img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox15.BackgroundImage = img;
+
+            img = pictureBox16.BackgroundImage;
+            img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            pictureBox16.BackgroundImage = img;
 
         }
     }
